ee1993194fc53e89be8d7a5e9f5867b12cfafa37
Ankur <ankurmandal.official@gmail.com>
2023-10-28 13:33:55 +0530

    learn about throw keyword

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
-
+        int age=12;
+       try {
+           if(age<18){
+               throw new RuntimeException("You are under age");
+           }
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
     }
 }
