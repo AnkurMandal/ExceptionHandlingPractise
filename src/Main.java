c961bab4fbd3c96ffa06f11d5bcfb8c172de57b6
Ankur <ankurmandal.official@gmail.com>
2023-10-28 22:36:05 +0530

    learn throws keyword

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
-        int age=124;
+        int age=12;
        try {
             if(checkAge(age)){
                 System.out.println("Congrats you are Adult now!!!");
@@ -10,11 +10,13 @@ public class Main {
            System.out.println(e.getMessage());
        }
     }
-    public static boolean checkAge(int age) {
+    public static boolean checkAge(int age) throws UnderAgeException {
 
         if(age<18) {
-            throw new UnderAgeException("You are under age");
+                throw new UnderAgeException("You are under age");
         }
+//        int a=3/0;
+
         return true;
     }
 }
